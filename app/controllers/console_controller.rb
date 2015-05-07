ca9c2ae619d5c1c922359795be41a58a49d17057
Masaki Kobayashi <makky.4d6b.3f5@gmail.com>
2015-05-07 16:40:44 +0900

    console#index

--- app/controllers/console_controller.rb
+++ app/controllers/console_controller.rb
@@ -1,6 +1,11 @@
 class ConsoleController < ApplicationController
   def index
-    
+    p params[:word]
+    if params[:word]
+      @subjects = Subject.where(Subject.arel_table[:title].matches("%#{params[:word]}%"))
+    else
+      @subjects = nil
+    end
   end
 
   def show
