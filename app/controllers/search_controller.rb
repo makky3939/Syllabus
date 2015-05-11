db051988a9f5fa6f5378f4624a998c308635a4c7
Masaki Kobayashi <makky.4d6b.3f5@gmail.com>
2015-05-11 13:00:30 +0900

    fix n + 1 problem

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
     if params[:word]
-      @subjects = Subject.where(Subject.arel_table[:title].matches("%#{params[:word]}%"))
+      @subjects = Subject.includes(:teachers).where(Subject.arel_table[:title].matches("%#{params[:word]}%"))
     else
       @subjects = nil
     end
