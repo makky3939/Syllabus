3906a91487f9427f7d66ce9c69568dd3d75da2c6
Masaki Kobayashi <makky.4d6b.3f5@gmail.com>
2015-05-11 13:10:55 +0900

    add validate

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    if params[:word]
+    if params[:word] && !params[:word].empty?
       @subjects = Subject.includes(:teachers).where(Subject.arel_table[:title].matches("%#{params[:word]}%"))
     else
       @subjects = nil
