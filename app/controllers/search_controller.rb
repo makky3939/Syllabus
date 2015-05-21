28b3faade93bf34cd45fd19242f48310a25ee40f
Masaki Kobayashi <makky.4d6b.3f5@gmail.com>
2015-05-21 09:41:33 +0900

    add escape method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,14 @@
 class SearchController < ApplicationController
   def index
     if params[:word] && !params[:word].empty?
-      @subjects = Subject.includes(:teachers).where(Subject.arel_table[:title].matches("%#{params[:word]}%"))
+      @subjects = Subject.includes(:teachers).where(Subject.arel_table[:title].matches(escape_like("%#{params[:word]}%")))
     else
       @subjects = nil
     end
+  end
+
+  private
+  def escape_like(string)
+    string.gsub(/[\\%_]/){|m| "\\#{m}"}
   end
 end
