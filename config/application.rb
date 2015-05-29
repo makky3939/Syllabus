96c72916801ba570d67d67757cba3b6ba44db23a
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2015-05-29 17:15:22 +0900

    change app name

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Syllabus
+module Visubus
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
