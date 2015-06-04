d2ea44c9955fc65b8152e87ec37c6041a2943084
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2015-06-04 14:09:31 +0900

    search teachers action

--- app/controllers/api/teachers_controller.rb
+++ app/controllers/api/teachers_controller.rb
@@ -1,5 +1,6 @@
 class Api::TeachersController < ApplicationController
   def index
-    render :json => []
+    @teacher = Teacher.select('id, name, face_filename').where('face_filename != ?', 'null').sample(3)
+    render json: @teacher
   end
 end
