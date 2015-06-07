65a9ae895c7cc865fff247da80bb84c348306dd7
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2015-06-08 02:19:45 +0900

    add columns

--- db/migrate/20150607171704_create_subject_voices.rb
+++ db/migrate/20150607171704_create_subject_voices.rb
@@ -1,7 +1,8 @@
 class CreateSubjectVoices < ActiveRecord::Migration
   def change
     create_table :subject_voices do |t|
-
+      t.column :subject_teacher_id, :integer, null: false
+      t.string :voice_filename
       t.timestamps null: false
     end
   end
